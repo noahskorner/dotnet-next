5ae44c369a0989056dd0ced3ebd692699b38204d
noah <noahskorner@gmail.com>
2022-08-27 12:44:05 -0500

    Commiting DateService

---
+++ dotnet/Services/Services/IDateService.cs
@@ -0,0 +1,16 @@
+﻿namespace Services.Services
+{
+    public interface IDateService
+    {
+        DateTimeOffset UtcNow();
+    }
+
+    public class DateService : IDateService
+    {
+        public DateTimeOffset UtcNow()
+        {
+            return DateTimeOffset.UtcNow;
+        }
+    }
+
+}
