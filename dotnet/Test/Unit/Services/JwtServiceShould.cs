5d1d4ecd0f3c0a77d8199256b2fe59a360714544
noah <noahskorner@gmail.com>
2022-08-27 12:45:17 -0500

    Added failing tests.

--- dotnet/Test/Unit/Services/JwtServiceShould.cs
+++ dotnet/Test/Unit/Services/JwtServiceShould.cs
@@ -7,7 +7,7 @@ namespace Test.Unit.Services
     public class JwtServiceShould
     {
         private const string _secretKey = "adcb5c6b680578bb7236661d291ef368c8c4c010551e01653d85e49d72cdae7dd3d70c8ca168b63931fece4dd59c4b5ee22647998df9b6f0409d254b2ac4321b";
-        private readonly IJwtService _sut = new JwtService(new JwtConfiguration() { Issuer = "https://localhost:5000", Audience = "https://localhost:5000" });
+        private readonly IJwtService _sut = new JwtService(new JwtConfiguration() { Issuer = "https://localhost:5000", Audience = "https://localhost:5000" }, new DateService());
 
         [Test]
         public void ReturnAToken()
